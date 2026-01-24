956b14dd9449686d2a99ad562f19d5fe4b18bf11
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2026-01-24 16:04:16 -0600

    feat: added in base components
    Co-authored-by: Savannah Walters <sav@savannah.lol>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CanvasComponent } from '../ui/canvas/canvas/canvas.component';
+import { ScriptComponent } from '../ui/script/script/script.component';
+import { TimelineComponent } from '../ui/timeline/timeline/timeline.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CanvasComponent, ScriptComponent, TimelineComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('quickboard');
