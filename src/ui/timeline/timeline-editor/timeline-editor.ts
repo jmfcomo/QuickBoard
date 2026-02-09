9f6a1fe09ecbc0937ce3d4a76fd74214957ba572
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2026-02-09 14:30:08 -0600

    chore: moved timeline functionality to editor component

--- src/ui/timeline/timeline-editor/timeline-editor.ts
+++ src/ui/timeline/timeline-editor/timeline-editor.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AppStore } from '../../../data/store/app.store';
 
 @Component({
   selector: 'app-timeline-editor',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './timeline-editor.css',
 })
 export class TimelineEditor {
+  readonly store = inject(AppStore);
 
+  addBoard() {
+    const newBoardId = this.store.addBoard();
+    this.store.setCurrentBoard(newBoardId);
+  }
+
+  selectBoard(boardId: string) {
+    this.store.setCurrentBoard(boardId);
+  }
+
+  deleteBoard(boardId: string) {
+    const boards = this.store.boards();
+    if (boards.length > 1) {
+      this.store.deleteBoard(boardId);
+      // Select another board if the deleted one was selected
+      if (this.store.currentBoardId() === boardId) {
+        this.store.setCurrentBoard(boards[0].id);
+      }
+    }
+  }
 }
--- src/ui/timeline/timeline/timeline.component.ts
+++ src/ui/timeline/timeline/timeline.component.ts
@@ -1,32 +1,11 @@
-import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
-import { AppStore } from '../../../data/store/app.store';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { TimelineEditor } from '../timeline-editor/timeline-editor';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TimelineEditor],
 })
-export class TimelineComponent {
-  readonly store = inject(AppStore);
-
-  addBoard() {
-    const newBoardId = this.store.addBoard();
-    this.store.setCurrentBoard(newBoardId);
-  }
-
-  selectBoard(boardId: string) {
-    this.store.setCurrentBoard(boardId);
-  }
-
-  deleteBoard(boardId: string) {
-    const boards = this.store.boards();
-    if (boards.length > 1) {
-      this.store.deleteBoard(boardId);
-      // Select another board if the deleted one was selected
-      if (this.store.currentBoardId() === boardId) {
-        this.store.setCurrentBoard(boards[0].id);
-      }
-    }
-  }
-}
+export class TimelineComponent {}
