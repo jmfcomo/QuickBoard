225217f0157cee937375d25ad30ed9d33ba0b10c
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2026-03-17 16:51:36 -0500

    chore: removing unused components

--- src/ui/dialogs/index.ts
+++ src/ui/dialogs/index.ts
@@ -1,5 +1,1 @@
-export { VersionDialogComponent } from './version-dialog/version-dialog.component';
-export type { VersionInfo } from './version-dialog/version-dialog.component';
-export { AboutDialogComponent } from './about-dialog/about-dialog.component';
-export type { AboutInfo } from './about-dialog/about-dialog.component';
 export { AboutWindowComponent } from './about-window/about-window.component';
