943f5e1e17695e7b2e064c0e2bf7237abf63c033
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2026-04-09 15:12:59 -0500

    fix: capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.quickboard.app',
   appName: 'QuickBoard',
-  webDir: 'dist',
+  webDir: 'dist/browser',
   server: {
     androidScheme: 'https',
   },
